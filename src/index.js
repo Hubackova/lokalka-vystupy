a853b2b07ecd500553b8355409a63d823fe0c244
Hubackova <lenka.h@atlas.cz>
2018-02-23 17:01:31 +0100

    lokalka provisory homepage

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 
 
 import App from './components/App';
+import LokalkaHomepage from './components/LokalkaHomepage';
 import './index.css';
 
 
 
 ReactDOM.render(
-  <App />,
+  <LokalkaHomepage />,
   document.getElementById('root')
 );
