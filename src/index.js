c9a168bb6085ae58b6b39f007124987948aabde2
Hubackova <lenka.h@atlas.cz>
2018-09-04 11:13:01 +0200

    Revert "lokalka provisory homepage"
    This reverts commit a853b2b07ecd500553b8355409a63d823fe0c244.

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import ReactDOM from 'react-dom';
 
 
 import App from './components/App';
-import LokalkaHomepage from './components/LokalkaHomepage';
 import './index.css';
 
 
 
 ReactDOM.render(
-  <LokalkaHomepage />,
+  <App />,
   document.getElementById('root')
 );
